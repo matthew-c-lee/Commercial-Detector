32aeb5469af1b934d2c57f1f6dc00cf293d1ad37
Matthew Lee <mail@matthewlee.name>
2025-05-18 21:23:40 -0500

    Finish unit test

--- tests/test_detect_commercials.py
+++ tests/test_detect_commercials.py
@@ -1,19 +1,10 @@
 from src.detect_commercials import detect_commercials, init_db, get_video_duration_seconds
 from pathlib import Path
 
-
 def test_detect_commercials():
-    db = init_db(db_path="tests/cache.db")
+    db = init_db(db_path="tests/test-cache.db")
     
     output_dir = Path("tests/output")
-    # detect_commercials(
-    #     conn=db,
-    #     video_path=Path("tests/commercials.mkv"),
-    #     output_dir=output_dir,
-    #     should_reprocess=True,
-    #     override_cache=False,
-    #     use_cached_llm_response=True
-    # )
     detect_commercials(
         conn=db,
         video_path=Path("tests/commercials.mkv"),
@@ -29,14 +20,10 @@ def test_detect_commercials():
         print(file, get_video_duration_seconds(file))
 
     expected_results = [
-        (Path("tests/output/now_thats_what_i_call_music_vol_3.mp4"), 60.093364),
         (Path("tests/output/honeycomb_cereal.mp4"), 30.113415),
-        (Path("tests/output/bump_1.mp4"), 5.389),
         (Path("tests/output/bump.mp4"), 5.389),
         (Path("tests/output/trix_cereal.mp4"), 30.130098),
-        (Path("tests/output/chuck_e_cheese.mp4"), 15.048366),
         (Path("tests/output/harcourt_learning_direct.mp4"), 60.160097),
-        (Path("tests/output/nickelodeon_snow_day_movie.mp4"), 45.378664),
         (Path("tests/output/nickelodeon_promo.mp4"), 60.160097),
     ]
 
