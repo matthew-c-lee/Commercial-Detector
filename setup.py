05e3c6d91df36f11457906d8a31196ccd55d4eef
Matthew Lee <mail@matthewlee.name>
2025-05-31 01:59:20 -0500

    Ruff formatting

--- setup.py
+++ setup.py
@@ -10,9 +10,7 @@ setup(
     name="detect-commercials",
     version="1.0.1",
     packages=find_packages(exclude=["tests*", "test*"]),
-
     python_requires=">=3.10, <3.13",
-    
     install_requires=parse_requirements("requirements.txt"),
     entry_points={
         "console_scripts": [
